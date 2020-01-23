e281f66aa43a27d085ee3884095149b42d370451
MarArek <arek.maruszczak@gmail.com>
2020-01-23 17:13:27 +0100

    Fixed sorting feeddict

--- parser.py
+++ parser.py
@@ -19,7 +19,7 @@ class Rss:
 
     def _update_rss_users(self):
         self.rss_users = self.db.get_users_feeds()
-        
+
     def get_user_rss_list(self, user_id):
         if user_id in self.rss_users:
             return list(self.rss_users[user_id])
@@ -58,7 +58,7 @@ class Rss:
             if user_id not in self.rss_users:
                 return True
             if rss_url in self.rss_users[user_id]:
-                self.db.delete_feed(user_id, rss_url) 
+                self.db.delete_feed(user_id, rss_url)
                 self._update_rss_feeds()
                 self._update_rss_users()
                 return True
@@ -78,14 +78,16 @@ class Rss:
                     rss_feed = feedparser.parse(rss_url)
                     if rss_feed is None:
                         continue
-                    _newest_entry = rss_feed.entries[0]
+                    _all_entries = rss_feed.entries
+                    sort_feed_dict(_all_entries)
+                    _newest_entry = _all_entries[0]
                     _tmp_date_parsed = rss_feed.feed.get('updated_parsed') or _newest_entry.get('published_parsed') # published_parsed is not always available
                     _publish_time_as_datetime = datetime(*_tmp_date_parsed[:6])
                     _current_hash = hashlib.md5((_newest_entry.link + _newest_entry.title).encode()).hexdigest()
                     if _current_hash == self.rss_feeds[rss_url]['hash']:
                         continue
                     elif _publish_time_as_datetime <= self.rss_feeds[rss_url]['publish_time']: # Protects from duplicates recieved from Telegram
-                        logging.info(f"[PARSER] NOT PROCESSION {self.rss_feeds[rss_url]}: Publish time - {_publish_time_as_datetime}; Saved time: {self.rss_feeds[rss_url]['publish_time']}")
+                        logging.info(f"[PARSER] NOT PROCESSING {self.rss_feeds[rss_url]}: Publish time - {_publish_time_as_datetime}; Saved time: {self.rss_feeds[rss_url]['publish_time']}")
                         continue
                     else:
                         logging.info(f"[PARSER] PROCESSING Statement for {self.rss_feeds[rss_url]}: Publish time - {_publish_time_as_datetime}; Saved time: {self.rss_feeds[rss_url]['publish_time']}")
@@ -94,7 +96,7 @@ class Rss:
                         _safe_markdown_title = _safe_markdown_parser(_newest_entry.title)
                         _safe_markdown_url = _safe_markdown_parser(_newest_entry.link)
                         _safe_feed_title = _safe_markdown_parser(self.rss_feeds[rss_url]['title'])
-                        _update_time = dateparser.parse(rss_feed.entries[0].updated).strftime(" %m/%d/%Y %H:%M:%S %Z ")
+                        _update_time = dateparser.parse(_all_entries[0].updated).strftime(" %m/%d/%Y %H:%M:%S %Z ")
                         msg = f"""
 -------------------------------------
 *{_safe_markdown_title}*
@@ -109,11 +111,11 @@ POSTED AT: `{_update_time}`
                         for user_id in self.rss_users:
                             if rss_url in self.rss_users[user_id]:
                                 resp = tg_bot.send_message(user_id, msg, parse_mode='Markdown', disable_notification=True)
-                                logging.debug("[RSS_PARSER] Successufully sent update")
+                                logging.info(f"[RSS_PARSER] Successufully sent update for {rss_url}")
             except (Exception) as e:
                 logging.error(f"[PARSER] {e}")
                 continue
-            time.sleep(self.lookup_window)        
+            time.sleep(self.lookup_window)
 
 def _utc_to_local(utc_dt):
     return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
@@ -131,3 +133,17 @@ def simple_parse(url):
             }
         else:
             return f
+
+def sort_feed_dict(entries):
+    if entries[0].get('updated_parsed') is None:
+        return
+    else:
+        size = len(entries)
+        for i in range(size):
+        	min_index = i
+        	for j in range(i + 1, size):
+        		if datetime(*entries[min_index]["updated_parsed"][:6]) < datetime(*entries[j]["updated_parsed"][:6]):
+        			min_index = j
+        	temp = entries[i]
+        	entries[i] = entries[min_index]
+        	entries[min_index] = temp
